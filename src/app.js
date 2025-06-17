ec2b9ed97e49ab1a1c433396aed28564db272973
Riyad Hasan <onlygobs0@gmail.com>
2025-06-17 10:14:12 +0600

    project completed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const nav = document.getElementById('nav-menu');
+const devNote = document.querySelector('.devNote');
 
 document.addEventListener('click', (e) => {
   if (e.target.closest('#nav-menu-toggle-btn')) {
@@ -7,5 +8,14 @@ document.addEventListener('click', (e) => {
   }
   if (!e.target.closest('#nav-menu') && nav.classList.contains('show')) {
     nav.classList.remove('show');
+    return;
+  }
+
+  if (e.target.closest('.devNote-btn')) {
+    devNote.classList.add('show');
+    return;
+  }
+  if (!e.target.closest('.devNote-content') && devNote.classList.contains('show')) {
+    devNote.classList.remove('show');
   }
 });
